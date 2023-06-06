80b751288c191a61a42c64951304f7722cb51441
123bhagesh <123bhagesh@gmail.com>
2023-06-07 01:49:29 +0530

    card comleted

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -6,9 +6,11 @@ import Schedule from "../Assets/Images/schedule_icon.png";
 import UserIcon from "../Assets/Images/user_icon.png";
 import Setting from "../Assets/Images/setting_icon.png";
 import { FaRegBell } from "react-icons/fa";
-import { BsSearch } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
-
+import TotalRevenu from "../Assets/Images/total_revenue.png";
+import TotalLikes from "../Assets/Images/total_likes.png";
+import TotalUsers from "../Assets/Images/total_users.png";
+import TotalTransactions from "../Assets/Images/total_transactions_icon.png";
 const sidebarData = [
   {
     image: DashboardIcon,
@@ -29,6 +31,33 @@ const sidebarData = [
   {
     image: Setting,
     tab: "Setting",
+  },
+];
+
+const totalData = [
+  {
+    image: TotalRevenu,
+    title: "Total Revenues",
+    totalNo: "$ 2,129,430",
+    bg:"#DDEFE0"
+  },
+  {
+    image: TotalTransactions,
+    title: "Total Transactions",
+    totalNo: "1,500",
+    bg:"#F4ECDD"
+  },
+  {
+    image: TotalLikes,
+    title: "Total Likes",
+    totalNo: "9721",
+    bg:"#EFDADA"
+  },
+  {
+    image: TotalUsers,
+    title: "Total Users",
+    totalNo: "2,129,430",
+    bg:"#DEE0EF"
   },
 ];
 export const Dashboard = () => {
@@ -55,12 +84,24 @@ export const Dashboard = () => {
             <div className={Style.searchDiv}>
               <input type="text" placeholder="Search..." />
               <button>
-                <BiSearch style={{color:"rgb(100,100,100)"}} />
+                <BiSearch style={{ color: "rgb(100,100,100)" }} />
               </button>
             </div>
-            < FaRegBell  />
+            <FaRegBell />
             <img src="" alt="" />
           </div>
+        </div>
+        <div className={Style.totalContainer}>
+            {totalData?.map((el, i) => (
+          <div className={Style.card} style={{backgroundColor:el.bg}}>
+            <div>
+                  <p>{el.title}</p>
+                  <h2>{el.totalNo}</h2>
+                </div>
+                <img src={el.image} alt="" />
+              </div>
+            ))}
+          
         </div>
       </div>
     </div>
