a19aa12f4cc7baf55a5e09a985ff129867303097
123bhagesh <123bhagesh@gmail.com>
2023-06-07 15:21:05 +0530

    env file added

--- src/Component/Auth/NextAuth.jsx
+++ src/Component/Auth/NextAuth.jsx
@@ -5,8 +5,8 @@ export default NextAuth({
     providers : [
         // Google Provider
         GoogleProvider({
-            clientId: "1049523505558-k5l62us0nbmtjpafva3t1rmjobpqj72f.apps.googleusercontent.com",
-            clientSecret: "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"
+            clientId: CLINT_ID,
+            clientSecret: CLIENT_SECRET
         })
     ]
 })
